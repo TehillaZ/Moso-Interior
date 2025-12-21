9cec67d43db6a15e032a785e7778013b0575e643
TehillaTech <tehillazc@gmail.com>
2025-12-21 19:06:26 +0200

    Update manager.js

--- Client/js/manager.js
+++ Client/js/manager.js
@@ -1,7 +1,7 @@
 // Fetch summary data from server
 async function loadDashboard() {
   try {
-    const res = await fetch('https://moso-interior-site.onrender.com/api', {
+    const res = await fetch('https://moso-interior-site.onrender.com/api/summary', {
     method: 'GET',
     credentials: 'include' 
     });
